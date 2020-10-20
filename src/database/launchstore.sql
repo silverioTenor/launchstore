c7dba14775dfe42763f4c9e4624f66988d72e2bb
silverioTenor <silverio_tenor@outook.com>
2020-10-20 12:11:51 -0300

    Remodelagem do DB, para uma versão mais simplificada do sistema.

--- src/database/launchstore.sql
+++ src/database/launchstore.sql
@@ -1,34 +1,8 @@
-CREATE TABLE "address" (
-  "id" SERIAL PRIMARY KEY,
-  "cep" text NOT NULL,
-  "street" text NOT NULL,
-  "district" text NOT NULL,
-  "city" text NOT NULL,
-  "uf" text NOT NULL
-);
-
-CREATE TABLE "user" (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL,
-  "rg" int NOT NULL,
-  "cpf" int NOT NULL,
-  "birth" timestamp NOT NULL,
-  "gender" text NOT NULL,
-  "address_id" int UNIQUE
-);
-
-CREATE TABLE "categorie" (
-  "id" SERIAL PRIMARY KEY,
-  "name" text NOT NULL
-);
-
 CREATE TABLE "product" (
   "id" SERIAL PRIMARY KEY,
-  "category_id" int UNIQUE,
-  "user_id" int UNIQUE,
-  "name" text NOT NULL,
   "brand" text NOT NULL,
   "model" text NOT NULL,
+  "cor" text NOT NULL,
   "description" text NOT NULL,
   "old_price" int,
   "price" int NOT NULL,
@@ -39,23 +13,15 @@ CREATE TABLE "product" (
 CREATE TABLE "storage" (
   "id" SERIAL PRIMARY KEY,
   "product_id" int UNIQUE,
-  "quantity" int DEFAULT 0,
-  "status" int DEFAULT 1
+  "quantity" int DEFAULT 0
 );
 
 CREATE TABLE "file" (
   "id" SERIAL PRIMARY KEY,
   "path" text NOT NULL,
-  "product_id" int UNIQUE,
-  "user_id" int UNIQUE
+  "product_id" int UNIQUE
 );
-
-ALTER TABLE "product" ADD FOREIGN KEY ("category_id") REFERENCES "categorie" ("id");
 
 ALTER TABLE "product" ADD FOREIGN KEY ("id") REFERENCES "storage" ("product_id");
 
-ALTER TABLE "file" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "file" ("user_id");
-
-ALTER TABLE "address" ADD FOREIGN KEY ("id") REFERENCES "user" ("address_id");
+ALTER TABLE "product" ADD FOREIGN KEY ("id") REFERENCES "file" ("product_id");

804d7e8e9e9bc91dac8115246b48dae8c6c5d00e
silverioTenor <silverio_tenor@outook.com>
2020-10-30 20:00:33 -0300

    Organização das rotas

--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,16 @@ const productController = require('./app/controllers/productController');
 
 routes.get("/", productController.index);
 
+// ================================ HOME ================================
 routes.get("/about", homeController.about);
 routes.get("/exchangesAndReturn", homeController.exchange);
 routes.get("/privacy", homeController.privacy);
 
+// ============================== PRODUCTS ==============================
 routes.get("/products/show/:id", productController.show);
 routes.get("/products/create", productController.create);
 routes.get("/products/update/:id", productController.update);
+
 routes.post("/products", multer.array("photos", 4), productController.post);
 routes.put("/products", multer.array("photos", 4), productController.put);
 routes.delete("/products", productController.delete);

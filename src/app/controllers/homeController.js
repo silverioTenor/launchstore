47bc707f13fcfc414d6315809811569bd5f77ef3
silverioTenor <silverio_tenor@outook.com>
2020-10-31 10:13:28 -0300

    Correção na identação.

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -1,11 +1,11 @@
 module.exports = {
-    about(req, res) {
-        return res.render("home/about");
-    },
-    exchange(req, res) {
-        return res.render("home/exchangeAndReturn");
-    },
-    privacy(req, res) {
-        return res.render("home/privacy");
-    }
+  about(req, res) {
+    return res.render("home/about");
+  },
+  exchange(req, res) {
+    return res.render("home/exchangeAndReturn");
+  },
+  privacy(req, res) {
+    return res.render("home/privacy");
+  }
 }
